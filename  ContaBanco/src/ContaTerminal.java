a2b02134bb02590bc2a6dc2be9005a2ba674122c
Joao Carvalho <vitorjoao.ncarvalho@gmail.com>
2025-03-14 22:33:40 -0300

    Todo o scanner foi entendido, o programa rodou sem erros

---  ContaBanco/src/ContaTerminal.java
+++  ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe seu nome: ");
+            String nomeCliente = scanner.next();
+        System.out.print("Informe sua Agencia: ");
+            String agencia = scanner.next();
+        System.out.print("Informe a sua conta: ");
+            int numeroConta = scanner.nextInt();
+        System.out.print("Informe o valor a ser depositado: ");
+            double saldoDeposito = scanner.nextDouble();            
+
+        System.out.print("Olá Sr(a)" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: "+ numeroConta + " e seu saldo de " + saldoDeposito + " já está disponível para saque!");
     }
 }
